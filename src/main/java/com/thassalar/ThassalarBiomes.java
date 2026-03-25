61e4b18a9abf74bf89c57b5ff2ae6b4193a1ca28
leoneldenotas33-cyber <leoneldenotas33@gmail.com>
2026-03-25 05:50:18 +0200

    Create ThassalarBiomes.java with biome registry keys

---
+++ src/main/java/com/thassalar/ThassalarBiomes.java
@@ -0,0 +1,15 @@
+package com.thassalar;
+
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.util.Identifier;
+
+public class ThassalarBiomes {
+    public static final RegistryKey<Biome> ASHEN_WASTES = RegistryKey.of(net.minecraft.registry.RegistryKeys.BIOME, new Identifier("worldofthassalar", "ashen_wastes"));
+    public static final RegistryKey<Biome> VOID_MARSH = RegistryKey.of(net.minecraft.registry.RegistryKeys.BIOME, new Identifier("worldofthassalar", "void_marsh"));
+    public static final RegistryKey<Biome> CRYSTALMERE = RegistryKey.of(net.minecraft.registry.RegistryKeys.BIOME, new Identifier("worldofthassalar", "crystalmere"));
+    public static final RegistryKey<Biome> BLOODWOOD_FOREST = RegistryKey.of(net.minecraft.registry.RegistryKeys.BIOME, new Identifier("worldofthassalar", "bloodwood_forest"));
+    public static final RegistryKey<Biome> SUNKEN_RUINS = RegistryKey.of(net.minecraft.registry.RegistryKeys.BIOME, new Identifier("worldofthassalar", "sunken_ruins"));
+    public static final RegistryKey<Biome> THORNFIELD = RegistryKey.of(net.minecraft.registry.RegistryKeys.BIOME, new Identifier("worldofthassalar", "thornfield"));
+    public static final RegistryKey<Biome> ASHFALL_PEAKS = RegistryKey.of(net.minecraft.registry.RegistryKeys.BIOME, new Identifier("worldofthassalar", "ashfall_peaks"));
+}
